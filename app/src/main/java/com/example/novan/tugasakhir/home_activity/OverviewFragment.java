a911f3cf6e6c4a4bc22939b4ddf5a4f330c7c07e
Novan Andhy <novanandhytrianto@gmail.com>
2017-03-02 12:06:17 +0700

    create circle progressbar change piechart on overview layout

--- app/src/main/java/com/example/novan/tugasakhir/home_activity/OverviewFragment.java
+++ app/src/main/java/com/example/novan/tugasakhir/home_activity/OverviewFragment.java
@@ -7,16 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.eralp.circleprogressview.CircleProgressView;
+import com.eralp.circleprogressview.ProgressAnimationListener;
 import com.example.novan.tugasakhir.R;
-import com.example.novan.tugasakhir.util.ChartItem;
-import com.example.novan.tugasakhir.util.PieChartItem;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.melnykov.fab.FloatingActionButton;
 import com.melnykov.fab.ScrollDirectionListener;
 
@@ -33,86 +33,32 @@ public class OverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.overview_layout, container, false);
+        view = inflater.inflate(R.layout.progressbar_circle, container, false);
 
-        ListView lv = (ListView) view.findViewById(R.id.listView1);
+        CircleProgressView itemProgressBar = (CircleProgressView) view.findViewById(R.id.circle_progress_view);
+        itemProgressBar.setProgressWithAnimation(20, 3000);
 
-        ArrayList<ChartItem> list = new ArrayList<ChartItem>();
 
-        // 30 items
-        for (int i = 0; i < 10; i++) {
-            list.add(new PieChartItem(generateDataPie(i + 1), getContext()));
-        }
 
-        ChartDataAdapter cda = new ChartDataAdapter(getContext(), list);
-        lv.setAdapter(cda);
-
-       FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab2);
-        fab.attachToListView(lv, new ScrollDirectionListener() {
-            @Override
-            public void onScrollDown() {
-
-            }
-
-            @Override
-            public void onScrollUp() {
-
-            }
-        });
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab2);
+//        fab.attachToListView(lv, new ScrollDirectionListener() {
+//            @Override
+//            public void onScrollDown() {
+//
+//            }
+//
+//            @Override
+//            public void onScrollUp() {
+//
+//            }
+//        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         return view;
-    }
-
-    /** adapter that supports 3 different item types */
-    private class ChartDataAdapter extends ArrayAdapter<ChartItem> {
-
-        public ChartDataAdapter(Context context, List<ChartItem> objects) {
-            super(context, 0, objects);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return getItem(position).getView(position, convertView, getContext());
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            // return the views type
-            return getItem(position).getItemType();
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 1; // we have 3 different item-types
-        }
-    }
-
-    /**
-     * generates a random ChartData object with just one DataSet
-     *
-     * @return
-     */
-    private PieData generateDataPie(int cnt) {
-
-        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
-
-        for (int i = 0; i < 2; i++) {
-            entries.add(new PieEntry((float) ((Math.random() * 70) + 30), "Quarter " + (i+1)));
-        }
-
-        PieDataSet d = new PieDataSet(entries, "");
-
-        // space between slices
-        d.setSliceSpace(2f);
-        d.setColors(ColorTemplate.VORDIPLOM_COLORS);
-
-        PieData cd = new PieData(d);
-        return cd;
     }
 }
