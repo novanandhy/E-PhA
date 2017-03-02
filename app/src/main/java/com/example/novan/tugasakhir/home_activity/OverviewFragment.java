e8c00e82dbabd8f71a57fa0b89b30cd2d78e45ed
Novan Andhy <novanandhytrianto@gmail.com>
2017-03-02 16:44:25 +0700

    create gridview on overvier

--- app/src/main/java/com/example/novan/tugasakhir/home_activity/OverviewFragment.java
+++ app/src/main/java/com/example/novan/tugasakhir/home_activity/OverviewFragment.java
@@ -4,24 +4,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.OvershootInterpolator;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.eralp.circleprogressview.CircleProgressView;
-import com.eralp.circleprogressview.ProgressAnimationListener;
 import com.example.novan.tugasakhir.R;
 import com.melnykov.fab.FloatingActionButton;
 import com.melnykov.fab.ScrollDirectionListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Novan on 01/02/2017.
@@ -29,36 +21,80 @@ import java.util.List;
 
 public class OverviewFragment extends Fragment {
     View view;
+    private RecyclerView recyclerView;
+    private Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.progressbar_circle, container, false);
+        context = getActivity().getApplicationContext();
+        view = inflater.inflate(R.layout.overview_layout, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.listView1);
 
-        CircleProgressView itemProgressBar = (CircleProgressView) view.findViewById(R.id.circle_progress_view);
-        itemProgressBar.setProgressWithAnimation(20, 3000);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(context,2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        CustomAdapter adapter = new CustomAdapter(context);
+        recyclerView.setAdapter(adapter);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab2);
+        fab.attachToRecyclerView(recyclerView, new ScrollDirectionListener() {
+            @Override
+            public void onScrollDown() {
+
+            }
+
+            @Override
+            public void onScrollUp() {
+
+            }
+        });
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
 
 
-//        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab2);
-//        fab.attachToListView(lv, new ScrollDirectionListener() {
-//            @Override
-//            public void onScrollDown() {
-//
-//            }
-//
-//            @Override
-//            public void onScrollUp() {
-//
-//            }
-//        });
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         return view;
     }
+
+    private static class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.Holder>{
+
+        private Context context;
+
+        public CustomAdapter(Context context){
+            this.context = context;
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(context).inflate(R.layout.progressbar_circle,parent,false);
+            return new Holder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+
+            holder.progressView.setProgressWithAnimation(20,3000);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+
+        public static class Holder extends RecyclerView.ViewHolder{
+
+            private CircleProgressView progressView;
+
+            public Holder(View itemView) {
+                super(itemView);
+                progressView = (CircleProgressView) itemView.findViewById(R.id.circle_progress_view);
+            }
+        }
+    }
+
 }
