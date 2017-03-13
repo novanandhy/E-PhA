6a4fc4885921ec1382bfb00e21f0f3d17709dcb3
Novan Andhy <novanandhytrianto@gmail.com>
2017-03-13 22:30:47 +0700

    create edit profile

---
+++ app/src/main/java/com/example/novan/tugasakhir/profile_activity/EditProfileActivity.java
@@ -0,0 +1,65 @@
+package com.example.novan.tugasakhir.profile_activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.novan.tugasakhir.R;
+import com.melnykov.fab.FloatingActionButton;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    EditText name, age;
+    CircleImageView photo;
+    FloatingActionButton button_photo;
+    Button button_submit;
+    private static final int RESULT_LOAD_IMAGE = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        name = (EditText) findViewById(R.id.edit_name_profile);
+        age = (EditText) findViewById(R.id.edit_age_profile);
+        photo = (CircleImageView) findViewById(R.id.image_edit_profile);
+        button_photo = (FloatingActionButton) findViewById(R.id.image_edit_profile_button);
+        button_submit = (Button) findViewById(R.id.submit_profile);
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+        button_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
--- app/src/main/java/com/example/novan/tugasakhir/profile_activity/ProfileFragment.java
+++ app/src/main/java/com/example/novan/tugasakhir/profile_activity/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.novan.tugasakhir.profile_activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.novan.tugasakhir.R;
+import com.melnykov.fab.FloatingActionButton;
 import com.rey.material.widget.Switch;
 
 import java.util.ArrayList;
@@ -25,14 +27,23 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 
 public class ProfileFragment extends Fragment {
-    private ListView lvHomePage;
     private View view;
-    private int hour, min;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab_profile);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(),EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
