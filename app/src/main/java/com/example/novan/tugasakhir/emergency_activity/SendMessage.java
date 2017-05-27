641e6f30026128080ba5333df400b0cb1889fb0c
Novan Andhy <novanandhytrianto@gmail.com>
2017-05-27 14:10:19 +0700

    get location and send sms

--- app/src/main/java/com/example/novan/tugasakhir/emergency_activity/SendMessage.java
+++ app/src/main/java/com/example/novan/tugasakhir/emergency_activity/SendMessage.java
@@ -4,14 +4,15 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
@@ -26,18 +27,23 @@ public class SendMessage implements GoogleApiClient.ConnectionCallbacks,
     Context context;
 
     GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
+    Location mLastLocation, mUpdateLocation, mBestLocation;
     LocationRequest mLocationRequest;
+    LocationManager locationManager;
 
     private String latitude, longitude;
+
+    private static final int TWO_MINUTES = 1000*60*2;
+    private static final int FIVE_SECONDS = 1000*5;
 
     public SendMessage(Context context) {
         this.context = context;
     }
 
     public void sendSMSByManager() {
-        createLocationRequest();
+
         buildGoogleApiClient();
+
     }
 
     private void buildGoogleApiClient() {
@@ -50,8 +56,10 @@ public class SendMessage implements GoogleApiClient.ConnectionCallbacks,
     }
 
     private void createLocationRequest() {
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
+        mLocationRequest.setInterval(5000);
         mLocationRequest.setFastestInterval(1000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
@@ -70,7 +78,31 @@ public class SendMessage implements GoogleApiClient.ConnectionCallbacks,
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
+        createLocationRequest();
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+//        LocationListener locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//                mUpdateLocation = location;
+//
+//                boolean status = isBetterLocation(mUpdateLocation, mLastLocation);
+//
+//                if(status){
+//                    Log.d("TAG","update location");
+//                    mBestLocation = mUpdateLocation;
+//                }else {
+//                    Log.d("TAG","last location");
+//                    mBestLocation = mLastLocation;
+//                }
+//
+//                Log.d("TAG","Best location latitude = "+mBestLocation.getLatitude());
+//                Log.d("TAG","Best location longitude = "+mBestLocation.getLongitude());
+//            }
+//        };
+//        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,locationListener);
+
         if(mLastLocation != null){
             latitude = String.valueOf(mLastLocation.getLatitude());
             longitude = String.valueOf(mLastLocation.getLongitude());
@@ -98,5 +130,53 @@ public class SendMessage implements GoogleApiClient.ConnectionCallbacks,
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    protected boolean isBetterLocation(Location updateLocation, Location lastLocation){
+        if(lastLocation == null){
+            //a new location is always better than no last location
+            return true;
+        }
+
+        //check if the updated location is newer or older than last location
+        long timeDelta = updateLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        //if it's been 2 minutes, the new loction likely used
+        //if the user is moved
+        if(isSignificantlyNewer){
+            return true;
+        }else if(isSignificantlyOlder){
+            return false;
+        }
+
+        //check if the new location is fix accurate or not
+        int accuracyDelta = (int) (updateLocation.getAccuracy() - lastLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        //check if the old and new location is from the same provider
+        boolean isSameProvider = isSameProvider(updateLocation.getProvider(), lastLocation.getProvider());
+
+        //determine location quality using a combination of timeliness and accuracy
+        if(isMoreAccurate){
+            return true;
+        }else if(isNewer && !isLessAccurate){
+            return true;
+        }else  if(isNewer && !isSignificantlyLessAccurate){
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isSameProvider(String update, String last) {
+        if(update == null){
+            return last == null;
+        }
+        return update.equals(last);
     }
 }
